ddce5db08c9b14bb58f74b3edaa04dc0930bf368
Berendoska <k-b-v@yandex.ru>
2022-08-10 13:40:12 +0300

    second

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+    public String name;
+    public String password;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public FormDate birthday;
+    // поле для имени
+    // добавьте другие поля
+}
